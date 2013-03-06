d3692854d8166e1fefbe48a7615726894b48a813
sjolinjohan180 <sjolinjohan180@gmail.com>
2013-03-07 00:44:26 +0100

    deleting files

--- CV-Johan-Johansson-Sjolin_files/contact.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-cvAngularApp.controller('ContactCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
-});
--- CV-Johan-Johansson-Sjolin_files/personal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-cvAngularApp.controller('PersonalCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
-});
